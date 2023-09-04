265eb1aa9f780cbf20bc24d0b49497dee5f00e78
Albert Colubi <albertmc89@gmail.com>
2023-09-04 16:54:10 +0200

    Create user model schema and types

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export interface PlayerStructure {
+  id: string;
+  name: string;
+  country: string;
+  age: number;
+  height: number;
+  goals: number;
+  games: number;
+  position: string;
+  image: string;
+}
+
+export interface UserStructure {
+  id: string;
+  name: string;
+  authId: string;
+}
