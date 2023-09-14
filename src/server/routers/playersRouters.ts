800eb6fe8f463d9d9db4c1b437e9065275228de5
Albert Colubi <albertmc89@gmail.com>
2023-09-14 14:45:46 +0200

    Add delete player router

--- src/server/routers/playersRouters.ts
+++ src/server/routers/playersRouters.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getPlayersController } from "../controllers/players/playersControllers.js";
+import {
+  deletePlayerByIdController,
+  getPlayersController,
+} from "../controllers/players/playersControllers.js";
 
 const playersRouter = express.Router();
 
 playersRouter.get("/", getPlayersController);
+playersRouter.delete("/:idPlayer", deletePlayerByIdController);
 
 export default playersRouter;
