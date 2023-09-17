2be0c3c7932455b5756e38b93228ad3cfc508002
Albert Colubi <albertmc89@gmail.com>
2023-09-17 16:23:22 +0200

    Add supertest getById endpoint

---
+++ src/server/routers/__tests__/getByIdEndpoint.test.ts
@@ -0,0 +1,52 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import admin from "firebase-admin";
+import mongoose from "mongoose";
+import request from "supertest";
+import connectToDataBase from "../../../database/connectToDatabase";
+import Player from "../../../database/models/Player";
+import { idPlayerMock, playerByIdMock } from "../../mocks/playersMock";
+import app from "../..";
+import { type DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
+import { mockId, userMock } from "../../mocks/usersMock";
+import User from "../../../database/models/User";
+jest.mock("firebase-admin");
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+  await connectToDataBase(server.getUri());
+
+  const token: Partial<DecodedIdToken> = {
+    uid: mockId,
+  };
+
+  admin.auth = jest.fn().mockReturnValue({
+    verifyIdToken: jest.fn().mockResolvedValue(token),
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await server.stop();
+});
+
+describe(`Given a GET '/players/:idPlayer' endpoint`, () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with status 200 and a player'", async () => {
+      const expectedStatusCode = 200;
+      const path = `/players/${idPlayerMock}`;
+      const playerById = playerByIdMock[0].name;
+
+      await Player.create(playerByIdMock);
+      await User.create(userMock);
+
+      const response = await request(app)
+        .get(path)
+        .set("Authorization", "Bearer token")
+        .expect(expectedStatusCode);
+
+      expect(response.body.player).toHaveProperty("name", playerById);
+    });
+  });
+});
